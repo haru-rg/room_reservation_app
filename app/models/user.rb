e7eb88a456b34ff1625b5b3ee664a56700c050fd
haru-rg <haruchama1223@outlook.jp>
2021-06-30 21:43:42 +0900

    userコントローラーを追加(home#indexが表示されなくなった)、name/introduction/imageカラムを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true #制限追記
+  validates :introduction, length: { maximum: 200 } #制限追記
+  validates :image, length: { maximum: 200 } #制限追記
 end
